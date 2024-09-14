0bf372c8a40846001bd8326ae8d6460be9fcfad5
Andres Alvarez <72172628+andresalvarezb@users.noreply.github.com>
2024-09-14 12:11:15 -0500

    feat: :construction: create a basic form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,11 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <h1>hi</h1>
+    <form action="" method="post">
+        <input type="text" name="" id="" placeholder="user" />
+        <input type="password" name="" id="" placeholder="password" />
+        <button type="submit">Log in</button>
+    </form>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
